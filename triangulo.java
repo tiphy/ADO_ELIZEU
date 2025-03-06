a5d6c82757235038e2043f27df40fc57852cee28
tiphany <111897484+tiphy@users.noreply.github.com>
2025-03-06 14:48:33 -0300

    Update triangulo.java

--- triangulo.java
+++ triangulo.java
@@ -1,23 +1,3 @@
-/*  Escreva um programa que leia o valor de 3 lados inteiros positivos (A,B e C) de um triângulo.
-No início do programa compare os lados para saber se é uma figura de três lados apenas ou se é um
-triângulo, Se qualquer um dos lados for maior ou igual a soma dos outros dois então a figura não é um
-triângulo. SE ( A >= (B+ C) ou B >=(A+C) ou C >= (B+A) ). Se for um triângulo, descubra o TIPO de
-triângulo: “equilátero”, “escaleno” ou “isósceles”. Imprima ao final o TIPO de triângulo. Verifique na
-internet para saber como identificar o TIPO de triângulo.
-
-
-valor de A, B e C
-se A >= (B + C) ou B >= (A + C) ou C >= (A + B)  
-
-se a soma de A, B e C for d 180 = triângulo
-se nao = nao é triangulo 
-
-equilatero = todos os lados iguais
-escaleno = todos os lados diferentes
-isósceles = dois lados iguais e um diferente
-
- */ 
-
 import java.util.Scanner;
 public class triangulo{
     public static void main(String[] args) {
